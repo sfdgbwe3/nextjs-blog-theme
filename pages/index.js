bc83ee22154b9a2134ffb142e5d8457b5bd587e4
sfdgbwe3 <165954732+sfdgbwe3@users.noreply.github.com>
2024-04-03 13:26:51 -0700

    sdfsd

--- pages/index.js
+++ pages/index.js
@@ -10,22 +10,9 @@ import SEO from '../components/SEO';
 
 export default function Index({ posts, globalData }) {
   return (
-    <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
       <main className="w-full">
         <image src="https://drive.google.com/file/d/1d_oEg9pJVYWcyyaJW2_06-dfFhoC8cOR/view?usp=sharing" alt="ff"></image>
       </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
-    </Layout>
   );
 }
 
